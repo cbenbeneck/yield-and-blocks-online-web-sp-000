6d9e027bc6ed046cea24b3a4ea1f6149f3638fdd
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-29 00:57:08 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,10 +7,10 @@ def hello_t(array)
     i = i + 1 
   end 
   
-  array 
-else 
-  puts "Hey! No block was given!"
-end
+    array 
+  else 
+   puts "Hey! No block was given!"
+  end
 end 
 
 # call your method here!
